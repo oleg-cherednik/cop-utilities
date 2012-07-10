b3f140d475c0c7864b4254e0cc2d0df88281cbfa
oleg.cherednik <oleg.cherednik@fe5acdd3-212e-a9da-ff12-a8d1c26e8094>
2012-07-10 22:54:59 +0000

    Add unit tests

---
+++ trunk/i18n/test/cop/i18n/Count.java
@@ -0,0 +1,47 @@
+/**
+ * <b>License</b>: <a href="http://www.gnu.org/licenses/lgpl.html">GNU Leser General Public License</a>
+ * <b>Copyright</b>: <a href="mailto:abba-best@mail.ru">Oleg Cherednik</a>
+ * 
+ * $Id$
+ * $HeadURL$
+ */
+package cop.i18n;
+
+import java.util.Locale;
+
+/**
+ * @author Oleg Cherednik
+ * @since 11.07.2012
+ */
+public enum Count implements Localizable {
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN;
+
+	/*
+	 * Localizable
+	 */
+
+	public String i18n() {
+		return LocaleStore.i18n(this, name());
+	}
+
+	public String i18n(Locale locale) {
+		return LocaleStore.i18n(this, name(), locale);
+	}
+
+	/*
+	 * static
+	 */
+
+	static {
+		LocaleStore.registerBundle(Count.class, LocaleStoreTest.PATH_I18N);
+	}
+}
