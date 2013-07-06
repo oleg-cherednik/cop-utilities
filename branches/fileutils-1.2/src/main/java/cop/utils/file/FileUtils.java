f5a5e26082c1dff91e4f37dd0f67cd3ab68f7a1e
oleg.cherednik <oleg.cherednik@fe5acdd3-212e-a9da-ff12-a8d1c26e8094>
2013-07-06 08:59:07 +0000

    [maven-release-plugin]  copy for branch fileutils-1.2

---
+++ branches/fileutils-1.2/src/main/java/cop/utils/file/FileUtils.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright © 2013 oleg.cherednik (http://code.google.com/u/oleg.cherednik/)
+ *
+ * The copyright of the computer program is the property of oleg.cherednik. The program may
+ * be used and/or copied in accordance with the terms and conditions of GNU Leser General Public License.
+ */
+/*
+ * $Id$
+ * $URL$
+ */
+package cop.utils.file;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Oleg Cherednik
+ * @since 06.07.2013
+ */
+public final class FileUtils {
+	private FileUtils() {
+	}
+
+	public static String getExtension(String filename) {
+		if (filename == null)
+			throw new NullPointerException("Filename is not set");
+		if (filename.contains(File.pathSeparator))
+			throw new IllegalArgumentException("Illegal filename: " + filename);
+
+		filename = filename.trim();
+		int pos = filename.lastIndexOf('.');
+		return pos > 0 ? filename.substring(pos + 1) : null;
+	}
+
+	public static File getRelativePath(File base, File file) {
+		String[] basePath = getAbsoluteFilePath(base);
+		String[] filePath = getAbsoluteFilePath(file);
+
+		if (!basePath[0].equals(filePath[0]))
+			return new File(filePath[0]);
+
+		int i = 0;
+		int j = 0;
+		final int maxBasePath = basePath.length;
+		final int maxFilePath = filePath.length;
+
+		StringBuilder buf = new StringBuilder();
+
+		for (int max = Math.max(maxBasePath, maxFilePath); i < max && j < max; i++, j++)
+			if (!basePath[i].equals(filePath[j]))
+				break;
+
+		for (; i < maxBasePath; i++)
+			buf.append("..").append(File.separator);
+		for (; j < maxFilePath - 1; j++)
+			buf.append(filePath[j]).append(File.separator);
+
+		buf.append(filePath[j]);
+
+		return new File(buf.toString());
+	}
+
+	public static String[] getAbsoluteFilePath(File file) {
+		if (file == null)
+			throw new NullPointerException("File is not set");
+		if (!file.isAbsolute())
+			throw new IllegalArgumentException("File is not absolute: " + file.getPath());
+
+		file = file.getAbsoluteFile();
+		File parent;
+		List<String> path = new ArrayList<>();
+
+		do {
+			if ((parent = file.getParentFile()) != null) {
+				path.add(file.getName());
+				file = parent;
+			} else
+				path.add(file.getPath());
+		} while (parent != null);
+
+		Collections.reverse(path);
+
+		return path.toArray(new String[path.size()]);
+	}
+}
